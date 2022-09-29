346dbd18a732449943de94c582891d39c5a4cfa7
192011315 <112461033+192011315@users.noreply.github.com>
2022-09-29 09:33:18 +0530

    Update d3.5.py

--- d3.5.py
+++ d3.5.py
@@ -1,10 +1,11 @@
-ba=int(input("Enter starting :"))
-b=int(input("Enter end :"))
-c=int(input("Numbers to skip:"))
-d=a
-while(a>0):
-    if(d<b):
-        print("Skipeed numbers :",d)
-        d=d+(c+1)
-    else:
-        break
+m1 = [list(map(int, input("Enter row: ").split(" ")))
+      for i in range(int(input("Enter Number of rows and col: ")))]
+
+m2 = [list(map(int, input("Enter row: ").split(" ")))
+      for i in range(int(input("Enter Number of rows and col :")))]
+
+print("Add Matrix:")
+r = [[m1[i][j] + m2[i][j]
+      for j in range(len(m1[0]))]
+      for i in range(len(m1))]
+print(r)
