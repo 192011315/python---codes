46fe5355e151aab96c708f929a95707a90b49b0a
192011315 <112461033+192011315@users.noreply.github.com>
2022-10-01 09:10:10 +0530

    Update d5.2.py

--- d5.2.py
+++ d5.2.py
@@ -1,13 +1,15 @@
 start = int(input("a="))
 end = int(input("b="))
-count = 0;
+count = 0
+if(start<end):
+    print("Composite Numbers between %d and %d: " % (start, end));
 
-print("Composite Numbers between %d and %d: " % (start, end));
-
-for start in range(start, end+1):
-    for i in range(1, start+1):
-        if(start % i == 0):
-            count += 1;
+    for start in range(start+1, end+1):
+        for i in range(1, start+1):
+            if(start % i == 0):
+                count += 1;
     if(count > 2):
        print(start, end=" ")
     count = 0
+else:
+    print("invalid input")
